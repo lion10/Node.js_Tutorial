330502c8b4089c2ee12c7aec35b9aca9c1f10c92
omar suliman abu sabha <ssomar2028@gmail.com>
2020-05-18 22:08:34 +0300

    Pipes and Pipe Chaining. (Readable,Writable and Transform Streams

--- streams.js
+++ streams.js
@@ -1,11 +1,23 @@
 const fs = require('fs');
+const zlib = require('zlib');
 
-const readStream = fs.createReadStream('./test1.txt','utf8');
-const writeSream = fs.createWriteStream('test2.txt')
+// 1st example:  to comprized the data
+const gzip = zlib.createGzip();
+
+// const readStream1 = fs.createReadStream('./test1.txt','utf8');
+// const writeStream1 = fs.createWriteStream('test2.txt.gz');
+
+// // send the data to writeStream file
+// // readStream1.pipe(writeStream1);
+
+// // compressed data to new file
+// readStream1.pipe(gzip).pipe(writeStream1);
 
 
-// I use readStream becuse it uses full buffer which need to be big enough ( esspicay when i have alot of data like  2 giga)
-readStream.on('data',(chunk)=> {
-    // console.log(chunk);
-    writeSream.write(chunk);
-})
+// 2nd example: 
+// .gz to make file compressed
+const readStream = fs.createReadStream('./test2.txt.gz');
+const writeStream = fs.createWriteStream('uncomprizedfile.txt')
+// to uncompressed the data
+const gunzip = zlib.createGunzip();
+readStream.pipe(gunzip).pipe(writeStream);
